1a5e09edec3510a49f19fbdb9af5e089ccf542e9
hezhi-lin <hezhi.lin.august@gmail.com>
2016-11-25 22:33:28 -0800

    UpdateBits Solution

---
+++ code/src/UpdateBits/Solution.java
@@ -0,0 +1,14 @@
+package UpdateBits;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	int insertBits(int M, int N, int i, int j){
+		return i;
+	}
+
+}
