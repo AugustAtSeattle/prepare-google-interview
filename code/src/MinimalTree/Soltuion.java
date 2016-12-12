944d4f36e87457f1ce0783e7ffbc44ba92b11080
hezhi-lin <hezhi.lin.august@gmail.com>
2016-12-11 17:56:54 -0800

    answer Solution

--- code/src/MinimalTree/Soltuion.java
+++ code/src/MinimalTree/Soltuion.java
@@ -4,6 +4,27 @@ package MinimalTree;
 // Question: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create
 // binary search tree with minimal height;
 
-public class Soltuion {
+class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+	
+}
 
+public class Soltuion {
+	TreeNode createMinimalBST(int array[]){
+		if(array == null) return null;
+		return createMinimalBST(array, 0, array.length - 1);
+	}
+
+	private TreeNode createMinimalBST(int[] array, int start, int end) {
+		if(start > end) return null;// to do
+		
+		int mid = start + (end - start) /2;
+		TreeNode node = new TreeNode();
+		node.value = array[mid];
+		node.left = createMinimalBST(array, start, mid -1);
+		node.right = createMinimalBST(array, mid + 1, end);
+		return node;
+	}
 }
