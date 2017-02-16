a9a9c268e107e51a0cefc0fe8bafe5cfd056d69b
hezhi-lin <hezhi.lin.august@gmail.com>
2017-02-16 12:25:04 -0800

    modify three sum closest solution

--- code/src/ThreeSumClosest/Solution.java
+++ code/src/ThreeSumClosest/Solution.java
@@ -1,5 +1,33 @@
 package ThreeSumClosest;
 
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length < 3) return 0;
+        int result = nums[0] + nums[1] + nums[2];
+        
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++){
+            int start = i + 1;
+            int end = nums.length - 1;
+            while(start < end){
+                int sum = nums[i] + nums[start] + nums[end];
+                
+                if(Math.abs(target - sum) < Math.abs(target - result)){
+                    result = sum;
+                }
+                if(sum > target){
+                    end--;
+                }else{
+                    start++;
+                }
+            }
+        }
+        
+        return result;
+    }
 }
+
+//Summary
+// Math.abs();
